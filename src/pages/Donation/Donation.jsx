2564380787adf5deb4707bc91eb6d6ff4d44c106
anfal11 <anfal15-14303@diu.edu.bd>
2023-09-26 05:22:14 +0600

    extra delete button part added

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -15,8 +15,17 @@ const Donation = () => {
             setNoFound("No donation found");
         }
     },[])
+
+    const deleteAll = () => {
+        localStorage.removeItem("donated");
+        setDonation([]);
+        setNoFound("No donation found");
+    }
     return (
         <div>
+        {
+            donation.length > 0 && <button onClick={deleteAll} className="block mx-auto bg-[#009444] rounded-md p-3 text-white font-medium"> Delete All </button>
+        }
             <div>
                 {
                     noFound ? <div><p className="h-[70vh] flex justify-center items-center text-7xl font-bold"> {noFound} </p></div> : <div className="mt-20 max-w-[1320px] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4 mb-20">
