ef40eef65eeccb0042b216727e4fa308d3a2d09e
anfal11 <anfal15-14303@diu.edu.bd>
2023-09-26 05:15:38 +0600

    donation route work has been done and data is now set from local storage

--- src/pages/Donation/Donation.jsx
+++ src/pages/Donation/Donation.jsx
@@ -1,7 +1,31 @@
+import { useEffect, useState } from "react";
+import DonationCard from "./DonationCard/DonationCard";
+
 const Donation = () => {
+
+    const [donation, setDonation] = useState([]);
+    const [noFound, setNoFound] = useState(false);
+
+    useEffect(() =>{
+        const donated = JSON.parse(localStorage.getItem("donated"));
+        if(donated){
+            setDonation(donated);
+        }
+        else {
+            setNoFound("No donation found");
+        }
+    },[])
     return (
         <div>
-            this is donation
+            <div>
+                {
+                    noFound ? <div><p className="h-[70vh] flex justify-center items-center text-7xl font-bold"> {noFound} </p></div> : <div className="mt-20 max-w-[1320px] mx-auto grid grid-cols-1 lg:grid-cols-2 gap-6 lg:gap-4 mb-20">
+                        {
+                            donation.map (data => <DonationCard key={data.id} data={data}></DonationCard>)
+                        }
+                    </div>
+                }
+            </div>
         </div>
     );
 };
