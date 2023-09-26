1f4911ee747385400d2be38a4958d802c66005c1
anfal11 <anfal15-14303@diu.edu.bd>
2023-09-27 05:11:39 +0600

    readme file updated

--- src/components/Header/NavBar/Logo.jsx
+++ src/components/Header/NavBar/Logo.jsx
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const Logo = () => {
   return (
     <div>
+      <Link to='/'>
       <img src="/src/assets/Logo.png" alt="logo" className="" />
+      </Link>
     </div>
   );
 };
